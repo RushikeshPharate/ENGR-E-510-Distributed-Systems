12bffb9600849e31b0ccdedb5f95fbd2f8a9cd03
Rushikesh Chandrakant Pharate <rpharate@silo.luddy.indiana.edu>
2023-04-01 15:18:08 -0400

    final

--- Distributed-Sequencer/client.py
+++ Distributed-Sequencer/client.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     server_port = int(sys.argv[2])
 
     server = xmlrpc.client.ServerProxy(f'http://{server_address}:{server_port}/')
-    for i in range(1, 2):
-        print(f"Current ID for request {i} is : {server.get_id()} ")
+    for i in range(1):
+        print(f"Current ID is : {server.get_id()} ")
 
     
 
